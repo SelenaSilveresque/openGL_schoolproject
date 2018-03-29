d04dc16bf821b43842fdebaf11843d0dfb717f81
SelenaSilveresque <37440907+SelenaSilveresque@users.noreply.github.com>
2018-03-29 09:54:26 +0700

    Update object.hpp

--- object.hpp
+++ object.hpp
@@ -5,6 +5,7 @@
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
+#define GLM_ENALBLE_EXPERIMENTAL
 #include <glm/gtx/vector_angle.hpp>
 #include <SOIL.h>
 
